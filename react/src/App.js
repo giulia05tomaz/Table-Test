db9b7fd8b5da92f17146a1d7c458edbfe053e00a
Giulia Moraes <giulia05tomaz@gmail.com>
2022-02-22 00:50:58 -0300

    page

--- react/src/App.js
+++ react/src/App.js
@@ -1,36 +1,10 @@
 import './App.css';
-import {useEffect, useState} from 'react'
-import PaginationComponent from './PaginationComponent';
-import PaginationSelector from './PaginationSelector';
 
 
 
 function App() {
 
-  const [itens, setItens] = useState([])
-  const [itensPerPage, setItensPerPage] = useState(10)
-  const [currentPage, setCurrentPage] = useState(0)
 
-  const pages = Math.ceil(itens.length / itensPerPage)
-  const startIndex = currentPage + itensPerPage;
-  const endIndex = startIndex + itensPerPage;
-  const currentItens = itens.slice(startIndex,endIndex)
-
-    useEffect(() => {
-      const fetchData = async () => {
-      const result = await fetch('tabela')
-        .then(response => response.json())
-        .then(data => data)
-
-        setItens(result)
-
-        }
-        fetchData()
-      }, [])
-
-      useEffect(() => {
-        setCurrentPage(0)
-      }, [itensPerPage])
 
   return (
 
